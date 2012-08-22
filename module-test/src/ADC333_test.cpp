c18a22ba58610870e7bd10ffd12ddc1b4ccadb20
Gulevich Vasili <V.V.Gulevich@mail.ru>
2012-08-22 18:03:41 +0700

    Extended options. Program now fails with NOT_Q erro for unknown reason.

--- module-test/src/ADC333_test.cpp
+++ module-test/src/ADC333_test.cpp
@@ -8,14 +8,18 @@
 #include <ADC333.h>
 #include <tools/lam_wait.h>
 
+
+
+#define HANDLE_ERROR(x, message) {int a = x; if (a & CAMAC_CC_ERRORS) cerr << message << ": " << CamacErrorPrinter(a) << endl; return 8;}
 using namespace std;
 int main(int argc, char * argv[]) {
 	CamacAddressParser address("k/0/0/0");
 	bool channels[ADC333::CHAN_COUNT] = {false};
-	bool cycle = false;
-	int opt;
+	bool cycle = false, manual = false;
+	unsigned gain = 0;
 
-	while ((opt = getopt(argc, argv, "hrca:")) != -1) {
+	int opt;
+	while ((opt = getopt(argc, argv, "hrmca:g:")) != -1) {
 		switch(opt) {
 			case 'a':
 				if (address.parse(optarg)) {
@@ -36,14 +40,30 @@ int main(int argc, char * argv[]) {
 			case 'r':
 				cycle = true;
 				break;
+			case 'm':
+				manual = true;
+				break;
+			case 'g': {
+				int tmp = atoi(optarg);
+				if (tmp < 0 || tmp > 3) {
+					cerr << "Gain should be in 0..3" << endl;
+					return 4;
+				}
+				gain = tmp;
+			}
 			default:
 				cout <<
 				argv[0] <<
 				" -a address [-h] [-c number [-c number ...]]" <<
 				"Options:\n"
+				"-m          - manual trigger. Press Enter to emulate trigger.\n"
 				" -a address - use this address\n"
-				" -c number  - enable a channel [1..4]\n"
-				" -h         - show help"
+				" -r         - cycle mode. Readout stops on signal.\n"
+				" -g [0..3]  - gain mode.\n"
+				" -c number  - enable a channel [1.."<< ADC333::CHAN_COUNT << "]\n"
+				" -h         - show help\n"
+				"In normal mode program waits for the hardware trigger, performs the measurement and dumps the data.\n"
+				"In cycle mode program measures in cycle, stops on the hardware trigger and dumps latest data measured."
 				<< endl;
 			return 0;
 		}
@@ -62,36 +82,58 @@ int main(int argc, char * argv[]) {
 		return 3;
 	}
 
-	module.EnableChannels(channels);
+	{
+		unsigned gains[ADC333::CHAN_COUNT];
+		for (int i = 0; i < ADC333::CHAN_COUNT; ++i) {
+			if (channels[i])
+				gains[i] = gain + 1;
+			else
+				gains[i] = 0;
+		}
+		module.EnableChannels(gains);
+	}
 
 	if (cycle) {
-		module.StartCycle();
+		HANDLE_ERROR(module.StartCycle(), "Failed to start cycle measurement");
+		if (manual) {
+			clog << "Press any key to stop measurement" << endl;
+			char ch;
+			cin >> ch;
+			module.Stop();
+		}
 	} else {
-		module.StartSingleRun();
+		HANDLE_ERROR(module.StartSingleRun(), "Failed to start measurement");
+		if (manual) {
+			clog << "Press any key to start measurement" << endl;
+			char ch;
+			cin >> ch;
+			module.Trigger();
+		}
 	}
 
-	df_timeout_t timeout = 100*1000;
-	rv = lam_wait(module, &timeout);
+	while(true) {
+		df_timeout_t timeout = 100*1000;
+		rv = lam_wait(module, &timeout);
 
-	if (rv & CAMAC_CC_ERRORS) {
-		cerr << "Error while waiting for LAM: " << CamacErrorPrinter(rv) << endl;
-		return 5;
-	}
+		if (rv & CAMAC_CC_ERRORS) {
+			cerr << "Error while waiting for LAM: " << CamacErrorPrinter(rv) << endl;
+			return 5;
+		}
 
-	if (!(rv & CAMAC_CC_BOOL)) {
-		cerr << "Timeout while waiting for LAM: " << CamacErrorPrinter(rv) << endl;
-		return 6;
+		if (rv & CAMAC_CC_BOOL) {
+			break;
+		}
 	}
 
 	vector<double> data[ADC333::CHAN_COUNT];
 	{
 		for (unsigned chIdx = 0; chIdx < ADC333::CHAN_COUNT; chIdx++) {
 			if (channels[chIdx]) {
-				module.Read(chIdx, data[chIdx]);
+				HANDLE_ERROR(module.Read(chIdx, data[chIdx]), "Failed to read channel");
 			}
 		}
 	}
-	int i = 0;
+	unsigned i = 0;
 	while(true) {
 		bool end = false;
 		bool first = true;
@@ -109,6 +151,7 @@ int main(int argc, char * argv[]) {
 			}
 		}
 		cout << "\n";
+		++i;
 	}
 
 	return 0;
